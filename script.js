44a82c189dd0264e142f78a0a48093596b4849d7
Carter Rodrigues <carterfilhorodrigues@gmail.com>
2026-04-02 02:10:40 -0300

    mudei o numero do site

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ if(maionese){
    let totalFinal = totalValor + taxaEntrega;
     mensagem += `%0ATotal: R$${totalFinal.toFixed(2)}`;
 
-    let numero = "5585998554871";
+    let numero = "5585992825170";
     window.open(`https://wa.me/${numero}?text=${mensagem}`);
 }
 
